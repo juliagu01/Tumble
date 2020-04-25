41001de2b5ce7c0ef790eafd2b51a733e3e6b040
amandalxu <44329340+amandalxu@users.noreply.github.com>
2020-04-24 23:32:43 -0700

    Screen Class

---
+++ Tumble/src/Screen.java
@@ -0,0 +1,42 @@
+/**
+ * Represents the super class of features all screens share
+ * @author Amanda Xu
+ * @version 4/24/20
+ * Credit to ProcessingScreenSwitching from Demos Folder
+ *
+ */
+
+public abstract class Screen {
+	
+	public final int WIDTH, HEIGHT;
+	
+	public Screen(int width, int height) {
+		this.WIDTH = width;
+		this.HEIGHT = height;
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+	
+	}
+	
+	public void mousePressed() {
+		
+	}
+	
+	public void mouseMoved() {
+		
+	}
+	
+	public void mouseDragged() {
+		
+	}
+	
+	public void mouseReleased() {
+		
+	}
+	
+}
